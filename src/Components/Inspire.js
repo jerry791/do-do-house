36a8d2ef53d9ea03ab3f26004bd84d89646f7773
jerry791 <65881812+jerry791@users.noreply.github.com>
2023-12-11 13:56:08 +0800

    delete unused model

--- src/Components/Inspire.js
+++ src/Components/Inspire.js
@@ -8,10 +8,6 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { gsap } from "gsap";
 const ThreeJSComponent = () => {
   const [radioValue, setRadioValue] = useState('1');
-  const [index, setIndex] = useState(0);
-  const handleSelect = (selectedIndex) => {
-    setIndex(selectedIndex);
-  };
 
   const radios = [
     { name: 'Bed Room', value: '1' },
@@ -19,7 +15,45 @@ const ThreeJSComponent = () => {
     { name: 'Living Room', value: '3' },
 
   ];
-  // const canvasRef = useRef();
+  const canvasRef = useRef(null);
+  useEffect(() => {
+    //create scene
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xffffff)
+    //setting size
+    const sizes = {
+      width: window.innerWidth,
+      height: window.innerHeight * 0.65,
+    }
+    //setting camera
+    const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 1, 5000)
+    camera.position.z = 5;
+    camera.position.x = 5
+    camera.position.y = 5
+    //setting render
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(sizes.width, sizes.height);
+    // use ref as a mount point of the Three.js scene instead of the document.body
+    canvasRef.current && canvasRef.current.appendChild(renderer.domElement);
+    //import gltf loader
+    const loader = new GLTFLoader();
+    loader.load('../../public/model/reality_room_edited/bedRoom3.glb', function (gltf) {
+      let mesh = gltf.scene;
+      // mesh.position.set(0,-11,0);
+      mesh.position.set(0, 0, 0);
+      scene.add(mesh);
+    }, undefined, function (error) {
+      console.error(error);
+    });
+    //create cube
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+
+    renderer.render(scene, camera);
+  }, []);
 
   // useEffect(() => {
   //   // Scene
@@ -28,23 +62,26 @@ const ThreeJSComponent = () => {
 
   //   // GLTF Loader
   //   const loader = new GLTFLoader();
-  //   loader.load(
-  //     '../3D_model/reality_room_edited/livingRoom2.glb',
-  //     (gltf) => {
-  //       let mesh = gltf.scene;
-  //       mesh.position.set(0, 0, 0);
-  //       scene.add(mesh);
-  //     },
-  //     undefined,
-  //     (error) => {
-  //       console.error(error);
-  //     }
-  //   );
-
+  //   // loader.load(
+  //   //   '../../public/model/reality_room_edited/livingRoom2.glb',
+  //   //   (gltf) => {
+  //   //     let mesh = gltf.scene;
+  //   //     mesh.position.set(0, 0, 0);
+  //   //     scene.add(mesh);
+  //   //   },
+  //   //   undefined,
+  //   //   (error) => {
+  //   //     console.error(error);
+  //   //   }
+  //   // );
+  //   var geometry = new THREE.BoxGeometry(1, 1, 1);
+  //   var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  //   var cube = new THREE.Mesh(geometry, material);
+  //   scene.add(cube);
   //   // Sizes
   //   const sizes = {
   //     width: window.innerWidth,
-  //     height: window.innerHeight,
+  //     height: window.innerHeight*0.65,
   //   };
 
   //   // Ambient Light
@@ -59,9 +96,12 @@ const ThreeJSComponent = () => {
   //   scene.add(camera);
 
   //   // Renderer
-  //   const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current });
+  //   // const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current });
+  //   const renderer = new THREE.WebGLRenderer();
   //   renderer.setSize(sizes.width, sizes.height);
   //   renderer.setPixelRatio(3);
+  //   // use ref as a mount point of the Three.js scene instead of the document.body
+  //   canvasRef.current && canvasRef.current.appendChild( renderer.domElement );
 
   //   // Controls
   //   const controls = new OrbitControls(camera, canvasRef.current);
@@ -74,7 +114,7 @@ const ThreeJSComponent = () => {
   //   // Resize
   //   const handleResize = () => {
   //     sizes.width = window.innerWidth;
-  //     sizes.height = window.innerHeight;
+  //     sizes.height = window.innerHeight*0.65;
 
   //     camera.aspect = sizes.width / sizes.height;
   //     camera.updateProjectionMatrix();
@@ -93,63 +133,63 @@ const ThreeJSComponent = () => {
 
   //   loop();
 
-  //   // GSAP Timeline
-  //   const tl = gsap.timeline({
-  //     defaults: { duration: 1.5 },
-  //   });
+  //   // // GSAP Timeline
+  //   // const tl = gsap.timeline({
+  //   //   defaults: { duration: 1.5 },
+  //   // });
 
-  //   tl.fromTo('.boxContainer', { y: '100%' }, { y: '0%' });
+  //   // tl.fromTo('.boxContainer', { y: '100%' }, { y: '0%' });
 
-  //   // Event Listeners
-  //   let isDownModel = false;
-  //   const dots = document.querySelectorAll('.dot');
+  //   // // Event Listeners
+  //   // let isDownModel = false;
+  //   // const dots = document.querySelectorAll('.dot');
 
-  //   const handleMouseDown = () => {
-  //     isDownModel = true;
-  //   };
+  //   // const handleMouseDown = () => {
+  //   //   isDownModel = true;
+  //   // };
 
-  //   const handleMouseUp = () => {
-  //     isDownModel = false;
-  //     dots.forEach((item) => {
-  //       item.style.background = '#fff';
-  //       item.style.boxShadow = 'rgba(0, 0, 0, 0.2) 0px 0px 0px 14px';
-  //     });
-  //   };
+  //   // const handleMouseUp = () => {
+  //   //   isDownModel = false;
+  //   //   dots.forEach((item) => {
+  //   //     item.style.background = '#fff';
+  //   //     item.style.boxShadow = 'rgba(0, 0, 0, 0.2) 0px 0px 0px 14px';
+  //   //   });
+  //   // };
 
-  //   const handleMouseLeave = () => {
-  //     isDownModel = false;
-  //     dots.forEach((item) => {
-  //       item.style.background = '#fff';
-  //       item.style.boxShadow = 'rgba(0, 0, 0, 0.2) 0px 0px 0px 14px';
-  //     });
-  //   };
+  //   // const handleMouseLeave = () => {
+  //   //   isDownModel = false;
+  //   //   dots.forEach((item) => {
+  //   //     item.style.background = '#fff';
+  //   //     item.style.boxShadow = 'rgba(0, 0, 0, 0.2) 0px 0px 0px 14px';
+  //   //   });
+  //   // };
 
-  //   const handleMouseMove = () => {
-  //     if (!isDownModel) {
-  //       dots.forEach((item) => {
-  //         item.style.background = '#fff';
-  //         item.style.boxShadow = 'rgba(0, 0, 0, 0.2) 0px 0px 0px 14px';
-  //       });
-  //     } else {
-  //       dots.forEach((item) => {
-  //         item.style.background = 'rgba(0, 0, 0, 0)';
-  //       });
-  //     }
-  //   };
+  //   // const handleMouseMove = () => {
+  //   //   if (!isDownModel) {
+  //   //     dots.forEach((item) => {
+  //   //       item.style.background = '#fff';
+  //   //       item.style.boxShadow = 'rgba(0, 0, 0, 0.2) 0px 0px 0px 14px';
+  //   //     });
+  //   //   } else {
+  //   //     dots.forEach((item) => {
+  //   //       item.style.background = 'rgba(0, 0, 0, 0)';
+  //   //     });
+  //   //   }
+  //   // };
 
-  //   canvasRef.current.addEventListener('mousedown', handleMouseDown);
-  //   canvasRef.current.addEventListener('mouseup', handleMouseUp);
-  //   canvasRef.current.addEventListener('mouseleave', handleMouseLeave);
-  //   canvasRef.current.addEventListener('mousemove', handleMouseMove);
+  // //   canvasRef.current.addEventListener('mousedown', handleMouseDown);
+  // //   canvasRef.current.addEventListener('mouseup', handleMouseUp);
+  // //   canvasRef.current.addEventListener('mouseleave', handleMouseLeave);
+  // //   canvasRef.current.addEventListener('mousemove', handleMouseMove);
 
   //   // Cleanup
-  //   return () => {
-  //     window.removeEventListener('resize', handleResize);
-  //     canvasRef.current.removeEventListener('mousedown', handleMouseDown);
-  //     canvasRef.current.removeEventListener('mouseup', handleMouseUp);
-  //     canvasRef.current.removeEventListener('mouseleave', handleMouseLeave);
-  //     canvasRef.current.removeEventListener('mousemove', handleMouseMove);
-  //   };
+  //   // return () => {
+  //   //   window.removeEventListener('resize', handleResize);
+  //   //   // canvasRef.current.removeEventListener('mousedown', handleMouseDown);
+  //   //   // canvasRef.current.removeEventListener('mouseup', handleMouseUp);
+  //   //   // canvasRef.current.removeEventListener('mouseleave', handleMouseLeave);
+  //   //   // canvasRef.current.removeEventListener('mousemove', handleMouseMove);
+  //   // };
   // }, []);
   return (
     <Container fluid>
@@ -206,172 +246,13 @@ const ThreeJSComponent = () => {
             </ButtonGroup>
           </Col>
         </Row>
-        {/* carousell */}
       </Container>
-      <Canvas className="webgl" />
+      {/* <canvas ref={canvasRef}/> */}
+      {/* <Canvas ref={canvasRef}/> */}
+      <div ref={canvasRef}></div>
     </Container>
 
   );
 };
 
 export default ThreeJSComponent;
-// function App() {
-//   const [radioValue, setRadioValue] = useState('1');
-//   const [index, setIndex] = useState(0);
-//   const handleSelect = (selectedIndex) => {
-//     setIndex(selectedIndex);
-//   };
-
-//   const radios = [
-//     { name: 'Bed Room', value: '1' },
-//     { name: 'Kitchen', value: '2' },
-//     { name: 'Living Room', value: '3' },
-
-//   ];
-//   const scene = new THREE.Scene();
-//   scene.background = new THREE.Color(0xffffff)
-//   // scene.background = new THREE.Color(0x000000)
-//   //gltf loader
-//   const loader = new GLTFLoader();
-
-//   loader.load('model/reality_room_edited/livingRoom2.glb', function (gltf) {
-
-//     let mesh = gltf.scene;
-//     // mesh.position.set(0,-11,0);
-//     mesh.position.set(0, 0, 0);
-//     scene.add(mesh);
-//   }, undefined, function (error) {
-
-//     console.error(error);
-
-//   });
-//   const sizes = {
-//     width: window.innerWidth,
-//     height: window.innerHeight,
-//   }
-//   //light
-//   const ambientLight = new THREE.AmbientLight(0xffffff, 2)
-//   scene.add(ambientLight)
-//   //camera
-//   //可見視野設定 : 45度。50度會導致扭曲(魚眼效果)
-//   //相機的縱橫比 : 800/600，也就是4:3。其他常見的有1:1、16:9
-//   //near clipping point:0.1 far clipping point : 100，表示相機最遠就到100，最近就到0.1
-//   const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 1, 5000)
-//   camera.position.x = 5
-//   camera.position.y = 5
-//   camera.position.z = 5
-//   // const helper = new THREE.CameraHelper(camera)
-//   scene.add(camera)
-//   //Render it
-//   const canvas = document.querySelector(".webgl")
-//   const renderer = new THREE.WebGLRenderer({ canvas })
-
-//   //define how big the canvas is
-//   renderer.setSize(sizes.width, sizes.height)
-//   renderer.render(scene, camera)
-//   renderer.setPixelRatio(3)
-
-//   //Resize
-//   //監聽視窗有沒有執行resize這個事件
-//   window.addEventListener('resize', () => {
-//     //update Sizes
-//     sizes.width = window.innerWidth;
-//     sizes.height = window.innerHeight;
-//     //update camera
-//     camera.aspect = sizes.width / sizes.height
-//     camera.updateProjectionMatrix()//.updateProjectionMatrix() 方法會根據相機的參數來更新投影矩陣，Must be called after any change of parameters.
-//     //rerender
-//     renderer.setSize(sizes.width, sizes.height)
-//   })
-//   return (
-//     <Container fluid>
-//       {/* navbar */}
-//       <Navbar bg="transparent" data-bs-theme="light" className='justify-content-around'>
-//         <Navbar.Brand href="/do-do-house/Home" className='me-0'>Do Do House</Navbar.Brand>
-//         <Nav activeKey="/do-do-house/Inspire">
-//           <Nav.Item >
-//             <Nav.Link href="/do-do-house/Home" className='me-5'>Home</Nav.Link>
-//           </Nav.Item>
-//           <Nav.Item>
-//             <Nav.Link href="/do-do-house/Shop" className='me-5'>Shop</Nav.Link>
-//           </Nav.Item>
-//           <Nav.Item>
-//             <Nav.Link href="/do-do-house/Inspire" className='me-5'>Inspire</Nav.Link>
-//           </Nav.Item>
-//           <Nav.Item>
-//             <Nav.Link href="/do-do-house/Contact us" className='me-5'>Contact us</Nav.Link>
-//           </Nav.Item>
-//         </Nav>
-//         <Nav>
-//           <Nav.Item>
-//             <Nav.Link href="/do-do-house/Cart">
-//               <img src='img/cart-dark.svg' alt='cart' width={'25px'} />
-//               <div className='cart-point'>
-//                 <p>0</p>
-//               </div>
-//             </Nav.Link>
-//           </Nav.Item>
-//         </Nav>
-//       </Navbar>
-//       {/* body */}
-//       <Container fluid>
-//         <Row className="justify-content-md-center">
-//           <Col md="auto"><h3 className='text-dark mt-5'>空間靈感</h3></Col>
-//         </Row>
-//         <Row className="justify-content-center mt-4">
-//           <Col md={4}>
-//             <ButtonGroup className='w-100'>
-//               {radios.map((radio, idx) => (
-//                 <ToggleButton
-//                   key={idx}
-//                   id={`radio-${idx}`}
-//                   type="radio"
-//                   variant="outline-secondary"
-//                   name="radio"
-//                   value={radio.value}
-//                   checked={radioValue === radio.value}
-//                   onChange={(e) => setRadioValue(e.currentTarget.value)}
-//                 >
-//                   {radio.name}
-//                 </ToggleButton>
-//               ))}
-//             </ButtonGroup>
-//           </Col>
-//         </Row>
-//         {/* carousell */}
-//       </Container>
-//       <Canvas className="webgl">
-
-//       </Canvas>
-//       {/* <Container fluid className='position-absolute bottom-0'>
-//         <Row className='mt-4 mb-5'>
-//           <Carousel
-//             className='mb-0'
-//             activeIndex={index}
-//             onSelect={handleSelect}
-//             variant="dark"
-//             indicators={false}
-//             interval={null}
-//             wrap={false}
-//             nextIcon={<img src='img/arrow-right.svg' alt='arrow-right' width={'100px'} />}
-//             prevIcon={<img src='img/arrow-left.svg' alt='arrow-left' width={'100px'} />}
-//           >
-//             <Carousel.Item>
-//               <canvas class="webgl"></canvas>
-//             </Carousel.Item>
-//             <Carousel.Item>
-
-//             </Carousel.Item>
-//             <Carousel.Item>
-
-//             </Carousel.Item>
-//           </Carousel>
-//         </Row>
-//       </Container> */}
-
-//     </Container>
-
-//   );
-// }
-
-// export default App;
